c21bcad6ebf554d2e9a8432be9319df506a77950
Primus Vekuh <primus.vekuh@gmail.com>
2024-06-13 21:20:22 -0400

    Update error.py

--- error.py
+++ error.py
@@ -1,1 +1,14 @@
-["773463581296.dkr.ecr.us-gov-west-1.amazonaws.com/docker-release/mms/atlas-backend/service-api:1711007767323-release-1.2.0-8"]
+
+None
+/Users/primus.vekuh@sentinelone.com/Documents/reports/save_formatted_version.py:42: FutureWarning: 'M' is deprecated and will be removed in a future version, please use 'ME' instead.
+  instances_over_time = df.set_index('instance_create_time').resample('M').size().reset_index()
+Traceback (most recent call last):
+  File "/Users/primus.vekuh@sentinelone.com/Documents/reports/save_formatted_version.py", line 42, in <module>
+    instances_over_time = df.set_index('instance_create_time').resample('M').size().reset_index()
+  File "/Users/primus.vekuh@sentinelone.com/Library/Python/3.9/lib/python/site-packages/pandas/core/generic.py", line 9771, in resample
+    return get_resampler(
+  File "/Users/primus.vekuh@sentinelone.com/Library/Python/3.9/lib/python/site-packages/pandas/core/resample.py", line 2050, in get_resampler
+    return tg._get_resampler(obj, kind=kind)
+  File "/Users/primus.vekuh@sentinelone.com/Library/Python/3.9/lib/python/site-packages/pandas/core/resample.py", line 2272, in _get_resampler
+    raise TypeError(
+TypeError: Only valid with DatetimeIndex, TimedeltaIndex or PeriodIndex, but got an instance of 'Index'
