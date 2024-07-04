1092fe9cd88fff71bfc8a7dc4bc349d17f600dec
Primus Vekuh <primus.vekuh@gmail.com>
2024-07-04 14:20:50 -0400

    Update error.py

--- error.py
+++ error.py
@@ -1,3 +1,6 @@
 Env	Instance Name	Engine	Engine Version	Status	Owner	Validated Exists As Of	Ticket	Major	Minor	Engine EOL	Days to EOSL	Creation Date	Age	Allocated Storage (GB)	Instance Size (GB)
 
 FedRAMP-RDS-Upgrades - Uber Sheet.csv
+
+
+https://towardsdatascience.com/easily-download-any-python-package-for-aws-lambda-layers-b8ffd43a26ee
