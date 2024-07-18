6f849dd9463ab1df9591435dd876a2e0d644a472
Primus Vekuh <primus.vekuh@gmail.com>
2024-07-18 11:28:15 -0400

    Update error.py

--- error.py
+++ error.py
@@ -1,5 +1,5 @@
 NEW_SUMMARIES = [
-    "New Story 1 - Base Image Update Required",
-    "New Story 2 - Base Image Update Required",
-    "New Story 3 - Base Image Update Required"
+    "Namespace: vig-app-prod Image Name: mgmt-entities-loader Age in Days: 486 Owner: VigApp",
+    "Namespace: vig-app-prod Image Name: vig-app-backend Age in Days: 486 Owner: VigApp",
+    "Namespace: apicurio-prod Image Name: apicurio-v225 Age in Days: 468 Owner: bd-infra"
 ]
